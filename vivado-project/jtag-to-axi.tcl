af8720fdd000a5648c7e5c8b8ff4b8bce8797c8d
Brad <bradley.whitlock@gmail.com>
2019-09-10 09:58:59 -0600

    converted hello world to list

--- vivado-project/jtag-to-axi.tcl
+++ vivado-project/jtag-to-axi.tcl
@@ -17,50 +17,12 @@ for {set i 0} {$i < 10} {incr i} {
 
 }
 
-create_hw_axi_txn write_uart_txn [get_hw_axis hw_axi_1] -type WRITE -address $UART_TX -len 1 -data {00000048} -force
-run_hw_axi write_uart_txn
-
-create_hw_axi_txn write_uart_txn [get_hw_axis hw_axi_1] -type WRITE -address $UART_TX -len 1 -data {00000065} -force
-run_hw_axi write_uart_txn
-
-create_hw_axi_txn write_uart_txn [get_hw_axis hw_axi_1] -type WRITE -address $UART_TX -len 1 -data {0000006C} -force
-run_hw_axi write_uart_txn
-
-create_hw_axi_txn write_uart_txn [get_hw_axis hw_axi_1] -type WRITE -address $UART_TX -len 1 -data {0000006C} -force
-run_hw_axi write_uart_txn
-
-create_hw_axi_txn write_uart_txn [get_hw_axis hw_axi_1] -type WRITE -address $UART_TX -len 1 -data {0000006F} -force
-run_hw_axi write_uart_txn
-
-create_hw_axi_txn write_uart_txn [get_hw_axis hw_axi_1] -type WRITE -address $UART_TX -len 1 -data {0000002C} -force
-run_hw_axi write_uart_txn
-
-create_hw_axi_txn write_uart_txn [get_hw_axis hw_axi_1] -type WRITE -address $UART_TX -len 1 -data {00000020} -force
-run_hw_axi write_uart_txn
-
-create_hw_axi_txn write_uart_txn [get_hw_axis hw_axi_1] -type WRITE -address $UART_TX -len 1 -data {00000057} -force
-run_hw_axi write_uart_txn
-
-create_hw_axi_txn write_uart_txn [get_hw_axis hw_axi_1] -type WRITE -address $UART_TX -len 2 -data {0000006F} -force
-run_hw_axi write_uart_txn
-
-create_hw_axi_txn write_uart_txn [get_hw_axis hw_axi_1] -type WRITE -address $UART_TX -len 2 -data {00000072} -force
-run_hw_axi write_uart_txn
-
-create_hw_axi_txn write_uart_txn [get_hw_axis hw_axi_1] -type WRITE -address $UART_TX -len 2 -data {0000006C} -force
-run_hw_axi write_uart_txn
-
-create_hw_axi_txn write_uart_txn [get_hw_axis hw_axi_1] -type WRITE -address $UART_TX -len 2 -data {00000064} -force
-run_hw_axi write_uart_txn
-
-create_hw_axi_txn write_uart_txn [get_hw_axis hw_axi_1] -type WRITE -address $UART_TX -len 2 -data {00000021} -force
-run_hw_axi write_uart_txn
-
-create_hw_axi_txn write_uart_txn [get_hw_axis hw_axi_1] -type WRITE -address $UART_TX -len 2 -data {0000000D} -force
-run_hw_axi write_uart_txn
-
-create_hw_axi_txn write_uart_txn [get_hw_axis hw_axi_1] -type WRITE -address $UART_TX -len 2 -data {0000000A} -force
-run_hw_axi write_uart_txn
+set STRING1 {H e l l o , W o r l d ! "\r" "\n"}
+foreach x $STRING1 {	;# Now loop and print...
+    set c [format %8.8X [scan $x %c]]
+    create_hw_axi_txn write_uart_txn [get_hw_axis hw_axi_1] -type WRITE -address $UART_TX -len 1 -data $c -force
+    run_hw_axi write_uart_txn
+}
 
 # At this point, we know the PL is up and working...
 #create_hw_axi_txn read_timer_txn [get_hw_axis hw_axi_1] -type read -address $TIMER_BASE -size 32 -force
