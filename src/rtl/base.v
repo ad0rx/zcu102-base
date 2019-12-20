1abeac2ebdd7d32c00eab19c5dc0e6dee4b9b958
Brad Whitlock <bradley.whitlock@gmail.com>
2019-12-20 11:49:19 -0700

    write txn started

---
+++ src/rtl/base.v
@@ -0,0 +1,187 @@
+// UG1037 Table A-5
+// AXI Spec Table B1-1
+//  A3.2
+//  All inputs are flopped
+//  no resets - relying on global FPGA reset of Xilinx system as recommended by X
+module base
+  #(
+    //parameter PROTOCOL     = "AXI4LITE",
+    //parameter HAS_BURST    = 0,
+    //parameter HAS_CACHE    = 0,
+    //parameter HAS_LOCK     = 0,
+    //parameter HAS_PROT     = 1,
+    //parameter HAS_QOS      = 0,
+    //parameter HAS_REGION   = 0,
+    //parameter HAS_WSTRB    = 1,
+    //parameter HAS_BRESP    = 1,
+    //parameter HAS_RRESP    = 1,
+    parameter P_ADDR_WIDTH = 8,
+    parameter P_DATA_WIDTH = 32,
+    parameter P_NUM_LED    = 8,
+    parameter P_NUM_SWITCH = 8,
+    parameter P_NUM_BUTTON = 5
+    )
+   (
+    /*AUTOARG*/
+    // Outputs
+    s_awready, s_wready, s_bvalid, s_bresp, s_arready, s_rvalid,
+    s_rdata, s_rresp, irq, leds, uart_txd, aux_resetn,
+    // Inputs
+    ACLK, ARESETN, s_awvalid, s_awaddr, s_awprot, s_wvalid, s_wdata,
+    s_wstrb, s_bready, s_arvalid, s_araddr, s_arprot, s_rready,
+    switches, buttons, uart_rxd
+    );
+
+   // Clock and Reset
+   (* X_INTERFACE_INFO = "xilinx.com:signal:clock:1.0 ACLK CLK" *)
+   (* X_INTERFACE_PARAMETER = "ASSOCIATED_BUSIF S_AXI, ASSOCIATED_RESET ARESETN" *)
+   input ACLK;
+
+   (* X_INTERFACE_INFO = "xilinx.com:signal:reset:1.0 ARESETN RST" *)
+   (* X_INTERFACE_PARAMETER = "POLARITY ACTIVE_LOW" *)
+   input ARESETN;
+
+   // Write Address Channel
+   (* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI AWVALID" *)
+   input s_awvalid;
+
+   // Write address ready
+   (* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI AWREADY" *)
+   output s_awready;
+
+   (* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI AWADDR" *)
+   input [P_ADDR_WIDTH-1:0] s_awaddr;
+
+   // Protection type
+   (* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI AWPROT" *)
+   input [2:0]		    s_awprot;
+
+   // Write Data Channel
+   // Write valid
+   (* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI WVALID" *)
+   input		    s_wvalid;
+
+   // Write ready
+   (* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI WREADY" *)
+   output		    s_wready;
+
+   // Write data
+   (* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI WDATA" *)
+   input [P_DATA_WIDTH-1:0] s_wdata;
+
+   // Write strobes
+   // Byte lanes
+   (* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI WSTRB" *)
+   input [(P_DATA_WIDTH/8)-1:0] s_wstrb;
+
+   // Write Response Channel
+   // Write response valid
+   (* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI BVALID" *)
+   output			s_bvalid;
+
+   // Write response ready
+   (* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI BREADY" *)
+   input			s_bready;
+
+   // Write response
+   (* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI BRESP" *)
+   output [1:0]			s_bresp;
+
+   // Read Address Channel
+   // Read address valid
+   (* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI ARVALID" *)
+   input			s_arvalid;
+
+   // Read address ready
+   (* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI ARREADY" *)
+   output			s_arready;
+
+   // Read address
+   (* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI ARADDR" *)
+   input [P_ADDR_WIDTH-1:0]	s_araddr;
+
+   // Protection type
+   (* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI ARPROT" *)
+   input [2:0]			s_arprot;
+
+   // Read Data Channel
+   // Read valid
+   (* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI RVALID" *)
+   output			s_rvalid;
+
+   // Read ready
+   (* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI RREADY" *)
+   input			s_rready;
+
+   // Read data
+   (* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI RDATA" *)
+   output [P_DATA_WIDTH-1:0]	s_rdata;
+
+   // Read response
+   (* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI RRESP" *)
+   output [1:0]			s_rresp;
+
+   // Interrupt Request
+   output			irq;
+
+   // General Purpose Output
+   output [P_NUM_LED-1:0]	leds;
+
+   // General Purpose Input
+   input [P_NUM_SWITCH-1:0]	switches;
+
+   input [P_NUM_BUTTON-1:0]	buttons;
+
+   // UART
+   (* X_INTERFACE_INFO = "xilinx.com:interface:uart:1.0 UART RxD" *)
+   input			uart_rxd;
+
+   (* X_INTERFACE_INFO = "xilinx.com:interface:uart:1.0 UART TxD" *)
+   output			uart_txd;
+
+   // Reset Output
+   // Able to reset PL from PS
+   // Needs another clock and input reset
+   // don't connect to ARESETN, only global reset
+   // implement as a counter so that it auto clears
+   (* X_INTERFACE_INFO = "xilinx.com:signal:reset:1.0 AUX_RESET RST" *)
+   (* X_INTERFACE_PARAMETER = "POLARITY ACTIVE_LOW" *)
+   output			aux_resetn;
+
+   reg [P_ADDR_WIDTH-1:0]	write_addr;
+   reg				s_awready_reg;
+   reg				s_awvalid_q;
+   reg [P_ADDR_WIDTH-1:0]	s_awaddr_q;
+
+   //top_level_assignments
+   assign s_awready = s_awready_reg;
+
+   //static_flops
+   always @ (posedge ACLK)
+     begin
+	s_awready_reg <= 1'b1;
+     end
+
+   always @ (posedge ACLK)
+
+     //if (ARESETN == 0) begin
+     //   write_addr    <= 0;
+     //   s_awvalid_reg <= 0;
+
+     //end else
+     begin
+
+	s_awvalid_q <= s_awvalid;
+	s_awaddr_q  <= s_awaddr;
+
+	if (s_awvalid_q == 1'b1) begin
+	   write_addr <= s_awaddr_q;
+	end
+
+     end
+
+
+
+
+
+endmodule // base_ip
---
+++ src/rtl/tb/base_tb.v
@@ -0,0 +1,160 @@
+// Simple directed test bench for the edge to level module
+//
+`timescale 1ns/1ps
+
+module base_tb;
+
+   localparam P_ADDR_WIDTH = 8;
+   localparam P_DATA_WIDTH = 32;
+   localparam P_NUM_LED    = 8;
+   localparam P_NUM_SWITCH = 8;
+   localparam P_NUM_BUTTON = 5;
+
+   reg ACLK;
+   reg ARESETN;
+
+   /*AUTOREG*/
+   reg s_awvalid;
+   reg s_wvalid;
+   reg s_bready;
+   reg s_arvalid;
+   reg s_rready;
+   reg uart_rxd;
+   reg [P_ADDR_WIDTH-1:0]         s_awaddr;
+   reg [2:0]			  s_awprot;
+   reg [P_DATA_WIDTH-1:0]	  s_wdata;
+   reg [(P_DATA_WIDTH/8)-1:0]	  s_wstrb;
+   reg [P_ADDR_WIDTH-1:0]	  s_araddr;
+   reg [2:0]			  s_arprot;
+   reg [P_NUM_SWITCH-1:0]	  switches;
+   reg [P_NUM_BUTTON-1:0]	  buttons;
+   //reg [P_NUM_LED-1:0]		  leds;
+
+   /*AUTOWIRE*/
+   // Beginning of automatic wires (for undeclared instantiated-module outputs)
+   wire			aux_resetn;		// From DUT of base.v
+   wire			irq;			// From DUT of base.v
+   wire [P_NUM_LED-1:0]	leds;			// From DUT of base.v
+   wire			s_arready;		// From DUT of base.v
+   wire			s_awready;		// From DUT of base.v
+   wire [1:0]		s_bresp;		// From DUT of base.v
+   wire			s_bvalid;		// From DUT of base.v
+   wire [P_DATA_WIDTH-1:0] s_rdata;		// From DUT of base.v
+   wire [1:0]		s_rresp;		// From DUT of base.v
+   wire			s_rvalid;		// From DUT of base.v
+   wire			s_wready;		// From DUT of base.v
+   wire			uart_txd;		// From DUT of base.v
+   // End of automatics
+
+   task axi4_lite_write_txn;
+      input [P_ADDR_WIDTH-1:0] write_address;
+      input [P_DATA_WIDTH-1:0] write_data;
+
+      begin
+
+	 // Set the address bus
+	 s_awaddr = write_address;
+
+	 // Set the VALID
+	 s_awvalid = 1'b1;
+
+	 // Wait for READY
+	 wait (s_awready == 1'b1);
+
+	 // Wait a cycle, data transfer occurs here
+	 @(posedge ACLK);
+
+	 // De-assert Valid
+	 s_awvalid = 1'b0;
+
+	 // Clear data bus
+	 s_awaddr = 0;
+
+      end
+   endtask // txn
+
+   // Clock and Reset
+   initial begin
+      ACLK     = 1;
+      ARESETN  = 0;
+
+     repeat (5)
+	begin
+	   @(posedge ACLK);
+	end
+
+      ARESETN = 1;
+
+   end
+
+   always
+     begin
+	#10 ACLK = !ACLK;
+     end
+
+   // Test
+   initial begin
+
+      // Initializations
+      s_awaddr  = {P_ADDR_WIDTH{1'b0}};
+      s_awvalid = 1'b0;
+      s_awprot  = 3'b0;
+
+
+      // Wait for Reset De-assert
+      repeat (10)
+	begin
+	   @(posedge ACLK);
+	end
+
+      // Transactions
+      axi4_lite_write_txn (8'hFF, 32'hDEAD_BEEF);
+   end
+
+   base
+       #(
+	 .P_ADDR_WIDTH (8),
+	 .P_DATA_WIDTH (32),
+	 .P_NUM_LED    (8),
+	 .P_NUM_SWITCH (8),
+	 .P_NUM_BUTTON (5)
+	 )
+   DUT
+     (
+       /*AUTOINST*/
+      // Outputs
+      .s_awready			(s_awready),
+      .s_wready				(s_wready),
+      .s_bvalid				(s_bvalid),
+      .s_bresp				(s_bresp[1:0]),
+      .s_arready			(s_arready),
+      .s_rvalid				(s_rvalid),
+      .s_rdata				(s_rdata[P_DATA_WIDTH-1:0]),
+      .s_rresp				(s_rresp[1:0]),
+      .irq				(irq),
+      .leds				(leds[P_NUM_LED-1:0]),
+      .uart_txd				(uart_txd),
+      .aux_resetn			(aux_resetn),
+      // Inputs
+      .ACLK				(ACLK),
+      .ARESETN				(ARESETN),
+      .s_awvalid			(s_awvalid),
+      .s_awaddr				(s_awaddr[P_ADDR_WIDTH-1:0]),
+      .s_awprot				(s_awprot[2:0]),
+      .s_wvalid				(s_wvalid),
+      .s_wdata				(s_wdata[P_DATA_WIDTH-1:0]),
+      .s_wstrb				(s_wstrb[(P_DATA_WIDTH/8)-1:0]),
+      .s_bready				(s_bready),
+      .s_arvalid			(s_arvalid),
+      .s_araddr				(s_araddr[P_ADDR_WIDTH-1:0]),
+      .s_arprot				(s_arprot[2:0]),
+      .s_rready				(s_rready),
+      .switches				(switches[P_NUM_SWITCH-1:0]),
+      .buttons				(buttons[P_NUM_BUTTON-1:0]),
+      .uart_rxd				(uart_rxd));
+
+endmodule // level_to_edge_tb
+// Local Variables:
+// verilog-library-directories:("." "../")
+// verilog-library-extensions:(".v" ".h")
+// End:
